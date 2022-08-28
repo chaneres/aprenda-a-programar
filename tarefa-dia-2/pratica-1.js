8e7f3f5d576ed113cfe6a9f89f35862feaff120a
Charlene Neres <charlene.neres@gmail.com>
2022-08-28 15:40:41 -0300

    update

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,11 +1,5 @@
-let nome 
-let email 
-let profissão 
-let idade 
-let viagemDosSonhos 
-
-nome = "Charlene"
-email = "charlene.neres@gmail.com"
-profissão = "Administradora"
-idade = "35"
-viagemDosSonhos = "Dubai"
+let nome = "Charlene"
+let email = "charlene.neres@gmail.com"
+let profissão = "Administradora"
+let idade = "35"
+let viagemDosSonhos = "Dubai"
