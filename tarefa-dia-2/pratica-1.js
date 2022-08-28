9427840536cd56ca767d8cf183bbdb008ddb225c
Charlene Neres <charlene.neres@gmail.com>
2022-08-28 16:10:56 -0300

    declarando variáveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email = "charlene.neres@gmail.com"
 let profissão = "Administradora"
 let idade = "35"
 let viagemDosSonhos = "Dubai"
+
+console.log(nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
+console.log(viagemDosSonhos)
