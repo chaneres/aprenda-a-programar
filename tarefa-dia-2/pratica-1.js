c000c6e730412d8639129691ba7f5352b23522c9
Charlene Neres <charlene.neres@gmail.com>
2022-08-28 15:36:56 -0300

    meu primeiro código

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,9 @@ let email
 let profissão 
 let idade 
 let viagemDosSonhos 
+
+nome = "Charlene"
+email = "charlene.neres@gmail.com"
+profissão = "Administradora"
+idade = "35"
+viagemDosSonhos = "Dubai"
