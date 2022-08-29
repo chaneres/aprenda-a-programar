35298e07d86567e9b971b18de0fb6e6cb0c993b8
Charlene Neres <charlene.neres@gmail.com>
2022-08-28 21:46:06 -0300

    numeros primos

---
+++ tarefa-dia-3/pratica-4.js
@@ -0,0 +1,13 @@
+let numero = 1557
+let i = 2
+let numeroPrimo = true
+
+if(numero % i == 0)
+numeroPrimo = false
+
+if (numeroPrimo)
+    console.log(`Sim, o número ${numero} é primo!`)
+else 
+    console.log(`Não, o número ${numero} não é primo!`)
+
+
